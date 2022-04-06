d0fdd285b2f98229258a2d2b9073c73d154164c6
user325 <user325@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-04-06 12:06:44 +0000

    Исправление ошибки Massive1-Massiv

--- src/main/java/Laba2/Program.java
+++ src/main/java/Laba2/Program.java
@@ -2,7 +2,7 @@ package Laba2;
 
 public class Program {
 	public static void progtam(int i) {
-		Massive1 Massive = new Massive1();
+		Massiv Massive = new Massiv();
 		Integer[] MassiveI = Massive.Massive2(i);
 		Sorting sorting = new Sorting();
 		sorting.sort(MassiveI);
