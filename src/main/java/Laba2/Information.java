26f169bc72c02b91000e98a8ea249f0156b3359d
user225 <user225@926748bc-2eb8-4e3c-9dda-e4fa1ab4064d>
2022-04-10 15:53:11 +0000

    добавлена возможность загрузки файла с данными для отображения этих данных на форме информации. данные заранее прописаны в .txt файл, выводятся на панели информации а так же в поле ввода радиуса и высоты

--- src/main/java/Laba2/Information.java
+++ src/main/java/Laba2/Information.java
@@ -2,16 +2,21 @@ package Laba2;
 
 import java.awt.event.*;
 import javax.swing.JOptionPane;
+import java.awt.event.ActionListener;
+
 
 public class Information implements ActionListener {
+	protected static String message = "";
     public void actionPerformed(ActionEvent e) {
-        String message = "ФИО и номера зачетных книжек\n";
+    	message += "ФИО и номера зачетных книжек\n";
         message += "-------------------------------\n";
         message += "Налигацкий Евгений | 20150096\n";
         message += "Аминев Алексей | 20130087\n";
         message += "Вяхирев Алексей | 20130485\n";
-        message += "Масленникова Ванесса | 20130840\n";
-        
+
+     
         JOptionPane.showMessageDialog(null, message , "Сообщение" , JOptionPane.PLAIN_MESSAGE);
+       
+        
     }
 }
